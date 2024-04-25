1ed16594b77afbe962d597af08395ea8d007bbca
raouf1616 <133696813+raouf1616@users.noreply.github.com>
2024-04-25 19:42:03 +0100

    Question 1 et 2

---
+++ app.js
@@ -0,0 +1,16 @@
+//Question 1 Raouf
+function calculerSomme(tableau) {
+    let somme = 0;
+    for (let i = 0; i < tableau.length; i++) {
+        somme += tableau[i];
+    }
+    return somme;
+}
+//Question 1 Raouf
+function calculerMoyenne(tableau) {
+    let somme = 0;
+    for (let i = 0; i < tableau.length; i++) {
+        somme += tableau[i];
+    }
+    return somme / tableau.length;
+}
