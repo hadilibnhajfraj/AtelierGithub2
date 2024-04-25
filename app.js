97980e5039ef94713cb76e1eb8b0e4963a7eb896
bhf hadil <hadil.ibnhajfraj@esprit.tn>
2024-04-25 19:30:07 +0100

    hadil commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// Fonction pour calculer la puissance d'un nombre
+function puissance(nombre, exposant) {
+  let resultat = 1;
+  for (let i = 0; i < exposant; i++) {
+    resultat *= nombre;
+  }
+  return resultat;
+}
+
+// Fonction pour calculer le PGCD de deux nombres
+function pgcd(a, b) {
+  if (b === 0) {
+    return a;
+  } else {
+    return pgcd(b, a % b);
+  }
+}
