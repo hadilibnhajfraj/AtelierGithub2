574a9a6150f4088e6f9cf6f8482230f5730fd654
bhf hadil <hadil.ibnhajfraj@esprit.tn>
2024-04-25 20:25:50 +0100

    hadil merge

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-// amina*
-//Question 9
-const tableauEntiers = [5, 2, 8, 1, 3, 7, 4];
+// Fonction pour calculer la puissance d'un nombre
+function puissance(nombre, exposant) {
+  let resultat = 1;
+  for (let i = 0; i < exposant; i++) {
+    resultat *= nombre;
+  }
+  return resultat;
+}
 
-tableauEntiers.sort((a, b) => a - b);
-
-console.log("Tableau trié en ordre croissant :", tableauEntiers);
-
-//Question 10
-// Fonction pour vérifier si une chaîne est un palindrome
-function estPalindrome(chaine) {
-  const chaineMinuscules = chaine.toLowerCase().replace(/ /g, "");
-  const chaineInverse = chaineMinuscules.split("").reverse().join("");
-  return chaineMinuscules === chaineInverse;
+// Fonction pour calculer le PGCD de deux nombres
+function pgcd(a, b) {
+  if (b === 0) {
+    return a;
+  } else {
+    return pgcd(b, a % b);
+  }
 }
