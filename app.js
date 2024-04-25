a718f4eb39b67fe5229edae6ad294f8206fb3a26
bhf hadil <hadil.ibnhajfraj@esprit.tn>
2024-04-25 20:00:44 +0100

    amina commit

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 // amina*
-//question 9
+//Question 9
 const tableauEntiers = [5, 2, 8, 1, 3, 7, 4];
 
 tableauEntiers.sort((a, b) => a - b);
 
 console.log("Tableau trié en ordre croissant :", tableauEntiers);
 
-
-
 //Question 10
 // Fonction pour vérifier si une chaîne est un palindrome
 function estPalindrome(chaine) {
-    const chaineMinuscules = chaine.toLowerCase().replace(/ /g, '');
-    const chaineInverse = chaineMinuscules.split('').reverse().join('');
-    return chaineMinuscules === chaineInverse;
+  const chaineMinuscules = chaine.toLowerCase().replace(/ /g, "");
+  const chaineInverse = chaineMinuscules.split("").reverse().join("");
+  return chaineMinuscules === chaineInverse;
 }
-
-
