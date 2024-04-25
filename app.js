e986b1a8b2902e1faac61bef104396f562695fe4
bhf hadil <hadil.ibnhajfraj@esprit.tn>
2024-04-25 22:42:10 +0100

    marwen commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// Fonction pour convertir une chaîne en majuscules
+function convertToUpperCase(str) {
+  return str.toUpperCase();
+}
+
+// Exemple d'utilisation
+const inputStrl = "hello world";
+const upperCaseStr = convertToUpperCase(inputStrl);
+console.log(upperCaseStr); // Affichera "HELLO WORLD"
+
+
+// Fonction pour calculer la longueur d'une chaîne
+function calculateStringLength(str) {
+  return str.length;
+}
+
+// Exemple d'utilisation
+const inputStr = "hello world";
+const strLength = calculateStringLength(inputStr);
+console.log(strLength); // Affichera 11
