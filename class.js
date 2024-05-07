b56e3d8fc01e2b32c1554cd475a0779f555acaf9
bhf hadil <hadil.ibnhajfraj@esprit.tn>
2024-05-07 19:52:03 +0100

    commit class

--- class.js
+++ class.js
@@ -1,4 +1,3 @@
-
 const tableauEntiers = [5, 2, 8, 1, 3, 7, 4];
 
 tableauEntiers.sort((a, b) => a - b);
@@ -6,9 +5,7 @@ tableauEntiers.sort((a, b) => a - b);
 //Question 10
 // Fonction pour vérifier si une chaîne est un palindrome
 function estPalindrome(chaine) {
-    const chaineMinuscules = chaine.toLowerCase().replace(/ /g, "");
-    const chaineInverse = chaineMinuscules.split("").reverse().join("");
-    return chaineMinuscules === chaineInverse;
-  }
-  
-
+  const chaineMinuscules = chaine.toLowerCase().replace(/ /g, "");
+  const chaineInverse = chaineMinuscules.split("").reverse().join("");
+  return chaineMinuscules === chaineInverse;
+}
